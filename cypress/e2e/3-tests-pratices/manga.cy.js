e0710615825e066d5e425b06da67fee82152a7aa
andres.leal <andres.leal@gmail.com>
2024-09-02 01:29:20 -0300

    feat : search manga title from list and store last chapter

--- cypress/e2e/3-tests-pratices/manga.cy.js
+++ cypress/e2e/3-tests-pratices/manga.cy.js
@@ -4,21 +4,25 @@ import { WebtoonPage } from "../../page-objects/webtoon-page";
 
 const webtoonPage = new WebtoonPage()
 
-it("should navigate to the page", () => {
+describe('check manga updates', () => {
+    it("should navigate to the page", () => {
 
-    cy.visit("https://manganato.com/")
-    const manga = "Dear shimazaki in the Peaceful Land";
-    const file={};
-    file.manga = manga;
-    cy.get('[id="search-story"]').type(`${manga}{enter}`);
-    //cy.get("li").contains(manga, {matchCase : false}).click();
+        cy.visit("https://manganato.com/")
+        const manga = ["Dear shimazaki in the Peaceful Land","One Piece", "Jujutsu Kaisen", "Ippon Again!","Mieruko-Chan"];
+        const file = [];
+        const obj = {};
 
-    cy.get('a.item-chapter').first().invoke("text").then((loggg) => {
-        file.chapter = loggg;
+        manga.forEach((mangaTitle, index) => {
+            cy.get('[id="search-story"]').type(`${mangaTitle}{enter}`);
+            //cy.get("li").contains(manga, {matchCase : false}).click();
+
+            cy.get('a.item-chapter').first().invoke("text").then((loggg) => {
+                file[index] = {manga : mangaTitle, chapter : loggg};
+            });
+        });
+        
         cy.writeFile("cypress/fixtures/mangaList.json",file)
-    })
+            
+    });
 
-         
-})
-
-//})
+});
